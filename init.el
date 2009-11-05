77382b6c01c67c4c9eb949c4951b330a43d6b817
Marc Sherry <msherry@gmail.com>
2009-11-05 14:33:37 -0800

    Improvement on ediff mode colors. Not perfect yet, and should maybe be moved into custom-faces, but it's a start for readability

--- init.el
+++ init.el
@@ -239,3 +239,16 @@
   "Keep the region active so we can do multiple shifts"
   (setq deactivate-mark nil))
 (ad-activate 'py-shift-region-right)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ediff-current-diff-B ((((class color) (min-colors 16)) (:background "brightmagenta" :foreground "blue"))))
+ '(ediff-even-diff-B ((((class color) (min-colors 16)) (:background "Grey" :foreground "black")))))
