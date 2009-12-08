b6df72247d263c6b4d98f943caae36c419a4450c
Marc Sherry <msherry@gmail.com>
2009-12-07 20:23:11 -0800

    Clarify what's necessary for etags to work

--- plugins/tags-funcs.el
+++ plugins/tags-funcs.el
@@ -1,20 +1,23 @@
 ;; Marc Sherry
 
+;; Depends on exuberant ctags being symlinked to etags
+(defvar tags-cmd "etags -R 2>/dev/null")
+
 (defun regen-tags ()
   "Regenerate the tags file for the current working directory"
   (interactive)
   (let ((tag-file (concat default-directory "TAGS")))
-    (shell-command "etags -R 2>/dev/null")
+    (shell-command tags-cmd)
     (visit-tags-table tag-file)))
 
 
-;; M-. to create a TAGS file if none exists
-(defadvice find-tag (before c-tag-file activate)
-   "Automatically create tags file."
-   (let ((tag-file (concat default-directory "TAGS")))
-     (unless (file-exists-p tag-file)
-       (shell-command "etags -R 2>/dev/null"))
-     (visit-tags-table tag-file)))
+;; ;; M-. to create a TAGS file if none exists
+;; (defadvice find-tag (before c-tag-file activate)
+;;    "Automatically create tags file."
+;;    (let ((tag-file (concat default-directory "TAGS")))
+;;      (unless (file-exists-p tag-file)
+;;        (shell-command tags-cmd))
+;;      (visit-tags-table tag-file)))
 
 
 (global-set-key (kbd "M-\"") 'regen-tags)
