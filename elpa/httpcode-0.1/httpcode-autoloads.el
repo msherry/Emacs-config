7f4ab18f27ad182246376c03b53ff6121b69246c
Marc Sherry <msherry@gmail.com>
2012-01-14 17:24:22 -0800

    New packages

---
+++ elpa/httpcode-0.1/httpcode-autoloads.el
@@ -0,0 +1,27 @@
+;;; httpcode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (hc) "httpcode" "httpcode.el" (20217 17336))
+;;; Generated autoloads from httpcode.el
+
+(autoload 'hc "httpcode" "\
+Display the meaning of an HTTP status code
+
+\(fn CODE)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("httpcode-pkg.el") (20217 17336 71218))
+
+;;;***
+
+(provide 'httpcode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; httpcode-autoloads.el ends here
---
+++ elpa/httpcode-0.1/httpcode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "httpcode" "0.1" "explains the meaning of an HTTP status code" (quote nil))
