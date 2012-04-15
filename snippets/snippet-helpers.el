f05f1f37124414630efbe0ae758f7374996cb751
Marc Sherry <msherry@ono.local>
2012-04-15 10:37:46 -0700

    TODO

--- snippets/snippet-helpers.el
+++ snippets/snippet-helpers.el
@@ -21,6 +21,9 @@ properly-structured cookbook -- <recipe>/recipes/<recipe_file>.rb"
   (file-name-nondirectory (directory-file-name (locate-dominating-file default-directory "recipes"))))
 
 (defun strip-leading-slash (text)
+  "Strips the first leading '/' from a filename path.
+
+TODO: This could be better done as a regex"
   (if (char-equal ?/ (string-to-char text))
       (substring text 1)
       text))
