b53a60bea584a112e593751fd440c6ca641c0a11
Marc Sherry <msherry@ono.local.tld>
2012-12-11 11:37:28 -0800

    fix tramp-mode checking

--- plugins/custom-utils.el
+++ plugins/custom-utils.el
@@ -5,18 +5,14 @@
 turning off flymake and auto-save when editing remote files via
 tramp."
   (and buffer-file-name
-       (file-writable-p (file-name-directory buffer-file-name))
-       (or (not (fboundp 'tramp-list-remote-buffers))
-           (not (member
-                 (current-buffer)
-                 (tramp-list-remote-buffers))))))
+       (file-is-local-p)
+       (file-writable-p (file-name-directory buffer-file-name))))
 
 
 (defun file-is-local-p ()
   "Determine if a file is local, versus being edited over tramp."
-  (or (not (fboundp 'tramp-list-remote-buffers))
-      (not (member
-            (current-buffer)
-            (tramp-list-remote-buffers)))))
+  (and buffer-file-name
+       (or (not (fboundp 'tramp-handle-file-remote-p))
+           (not (tramp-handle-file-remote-p buffer-file-name)))))
 
 (provide 'custom-utils)
