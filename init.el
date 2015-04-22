ebc1e9fced6836e7f43c31648b1fa6969127917e
Marc Sherry <msherry@gmail.com>
2015-04-21 22:50:40 -0700

    update package sources

--- init.el
+++ init.el
@@ -101,13 +101,9 @@ started from a shell."
 ;; Configure ELPA (package loader)
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ; Others'
