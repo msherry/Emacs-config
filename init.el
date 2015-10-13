a90e0f7b0a2a06ed2b528cd30b3a80f34e07ef06
Marc Sherry <msherry@gmail.com>
2015-10-13 11:54:49 -0700

    Default colors in magit log view

--- init.el
+++ init.el
@@ -593,6 +593,7 @@ http://blogs.fluidinfo.com/terry/2011/11/10/emacs-buffer-mode-histogram/"
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(gc-cons-threshold 100000000)
  '(global-eldoc-mode nil)
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate")))
  '(magit-push-always-verify nil)
  '(magit-tag-arguments (quote ("--annotate")))
  '(org-agenda-clockreport-parameter-plist (quote (:link t :maxlevel 3)))
