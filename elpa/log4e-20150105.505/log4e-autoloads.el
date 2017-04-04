535e3e1337714c2866f6e971f40902c58569661b
Marc Sherry <msherry@gmail.com>
2017-04-03 17:21:46 -0700

    Package updates

--- elpa/log4e-20150105.505/log4e-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; log4e-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "log4e" "log4e.el" (0 0 0 0))
-;;; Generated autoloads from log4e.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "log4e" '("log4e")))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; log4e-autoloads.el ends here
