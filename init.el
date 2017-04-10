527f9e4d3eafd4cf8f1f2cacba2a5e69a99ecfd2
Marc Sherry <msherry@gmail.com>
2017-04-10 01:02:05 -0700

    Hide auto-revert messages

--- init.el
+++ init.el
@@ -531,6 +531,7 @@ http://blogs.fluidinfo.com/terry/2011/11/10/emacs-buffer-mode-histogram/"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-verbose nil)
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
