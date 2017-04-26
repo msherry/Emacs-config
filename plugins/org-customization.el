38a727d8a74768e8d4bd8bd106bf078b0960758b
Marc Sherry <msherry@gmail.com>
2017-04-26 13:05:09 -0700

    IDO settings for org

--- plugins/org-customization.el
+++ plugins/org-customization.el
@@ -99,18 +99,28 @@
 (setq org-agenda-start-on-weekday 0)
 (setq org-log-done t)
 ;; http://doc.norang.ca/org-mode.html
-;; Targets include this file and any file contributing to the agenda - up
-;; to 9 levels deep
+; Targets include this file and any file contributing to the agenda - up to 9 levels deep
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                  (org-agenda-files :maxlevel . 9))))
-;; Use full outline paths for refile targets - we file directly with IDO
+
+; Use full outline paths for refile targets - we file directly with IDO
 (setq org-refile-use-outline-path t)
-;; Targets complete directly with IDO
+
+; Targets complete directly with IDO
 (setq org-outline-path-complete-in-steps nil)
-;; Allow refile to create parent tasks with confirmation
+
+; Allow refile to create parent tasks with confirmation
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
-;; Use IDO for both buffer and file completion and ido-everywhere to t
+
+; Use IDO for both buffer and file completion and ido-everywhere to t
 (setq org-completion-use-ido t)
+(setq ido-max-directory-size 100000)
+; Use the current window when visiting files and buffers with ido
+(setq ido-default-file-method 'selected-window)
+(setq ido-default-buffer-method 'selected-window)
+; Use the current window for indirect buffer display
+(setq org-indirect-buffer-display 'current-window)
+
 (setq org-agenda-time-grid '((daily today today)
                              #("----------------" 0 16 (org-heading t))
                              (800 1000 1200 1400 1600 1800 2000)))
