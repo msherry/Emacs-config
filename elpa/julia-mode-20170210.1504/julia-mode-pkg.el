ffa7b01dc2db3d210698d22f98113115866bd3dd
Marc Sherry <msherry@gmail.com>
2017-05-01 13:05:20 -0700

    Package upgrades

--- elpa/julia-mode-20170210.1504/julia-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "julia-mode" "20170210.1504" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
