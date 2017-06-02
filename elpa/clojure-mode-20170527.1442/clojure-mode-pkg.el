be5fb6fd871664598dc0520a1d8d1ef0dc50a481
Marc Sherry <msherry@gmail.com>
2017-06-01 21:58:24 -0700

    Package upgrades

---
+++ elpa/clojure-mode-20170527.1442/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20170527.1442" "Major mode for Clojure code" '((emacs "24.4")) :commit "6e966d57578bd1d30f2986b7aa8e222baa59a11c" :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
--- elpa/python-mode-20170429.55/python-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "python-mode" "20170429.55" "Python major mode" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
