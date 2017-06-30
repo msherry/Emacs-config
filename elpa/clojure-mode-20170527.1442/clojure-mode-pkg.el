53295e494b2d0f9fb988a6454a4426ce3d5bbda9
Marc Sherry <msherry@gmail.com>
2017-06-29 17:57:26 -0700

    Package upgrades

--- elpa/clojure-mode-20170527.1442/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20170527.1442" "Major mode for Clojure code" '((emacs "24.4")) :commit "6e966d57578bd1d30f2986b7aa8e222baa59a11c" :url "http://github.com/clojure-emacs/clojure-mode" :keywords '("languages" "clojure" "clojurescript" "lisp"))
---
+++ elpa/ess-20170603.817/ess-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ess" "20170603.817" "Emacs Speaks Statistics"
+  '((julia-mode "0.3"))
+  :url "http://ess.r-project.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
