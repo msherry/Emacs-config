e9717dd5c6b0a1cb00264bdcdd82927aa6a448cb
Marc Sherry <msherry@gmail.com>
2017-07-26 17:25:20 -0700

    Fix packages that I broke, switch to pycheckers package, enable org-babel SQL

---
+++ elpa/ess-20170726.609/ess-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ess" "20170726.609" "Emacs Speaks Statistics"
+  '((julia-mode "0.3"))
+  :url "http://ess.r-project.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/flycheck-pycheckers-20170726.1714/flycheck-pycheckers-pkg.el
@@ -0,0 +1,8 @@
+(define-package "flycheck-pycheckers" "20170726.1714" "multiple syntax checker for Python"
+                '((flycheck "0.18"))
+                :keywords
+                '("convenience" "tools" "languages")
+                :url "https://github.com/msherry/flycheck-pycheckers")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
