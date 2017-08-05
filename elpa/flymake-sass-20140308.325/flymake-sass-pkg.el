ac5bf1531810c1c3ef500a2af14822bbb9178371
Marc Sherry <msherry@gmail.com>
2017-08-04 22:59:22 -0700

    Package upgrades

--- elpa/flymake-sass-20140308.325/flymake-sass-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-sass" "20140308.325" "Flymake handler for sass and scss files" '((flymake-easy "0.1")) :url "https://github.com/purcell/flymake-sass")
--- elpa/fxrd-mode-20170125.228/fxrd-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "fxrd-mode" "20170125.228" "Major mode for editing fixed field width files"
-  '((s "1.2"))
-  :url "https://github.com/msherry/fxrd-mode" :keywords
-  '("convenience"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
