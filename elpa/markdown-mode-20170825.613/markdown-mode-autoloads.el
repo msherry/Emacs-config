ca9b777fcda84af0e97cc2684178764a972a6ed7
Marc Sherry <msherry@gmail.com>
2017-08-25 10:01:26 -0700

    Package updates

--- elpa/markdown-mode-20170825.613/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20170825.613/markdown-mode-autoloads.el
@@ -1,7 +1,10 @@
 ;;; markdown-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (0 0 0 0))
 ;;; Generated autoloads from markdown-mode.el
@@ -28,5 +31,6 @@ Major mode for editing GitHub Flavored Markdown files.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; markdown-mode-autoloads.el ends here
---
+++ elpa/markdown-mode-20170825.613/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170825.613" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "735fe6d1f79f5ff441f9559dc57831a566d21f60" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
