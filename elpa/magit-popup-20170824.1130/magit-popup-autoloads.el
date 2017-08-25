cdbace7f1d095f9229e16de6617b0bd2dc95c39c
Marc Sherry <msherry@gmail.com>
2017-08-24 19:42:20 -0700

    Package updates

--- elpa/magit-popup-20170824.1130/magit-popup-autoloads.el
+++ elpa/magit-popup-20170824.1130/magit-popup-autoloads.el
@@ -1,10 +1,7 @@
 ;;; magit-popup-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "magit-popup" "magit-popup.el" (0 0 0 0))
 ;;; Generated autoloads from magit-popup.el
@@ -21,6 +18,5 @@
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; magit-popup-autoloads.el ends here
--- elpa/markdown-mode-20170824.1038/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20170824.1038/markdown-mode-autoloads.el
@@ -1,10 +1,7 @@
 ;;; markdown-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (0 0 0 0))
 ;;; Generated autoloads from markdown-mode.el
@@ -31,6 +28,5 @@ Major mode for editing GitHub Flavored Markdown files.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; markdown-mode-autoloads.el ends here
