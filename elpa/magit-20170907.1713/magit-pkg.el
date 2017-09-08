e3279b5fefb419483c747b8761920bd8156e0fde
Marc Sherry <msherry@gmail.com>
2017-09-08 10:35:14 -0700

    Package upgrades

--- elpa/magit-20170907.1713/magit-pkg.el
+++ elpa/magit-20170907.1713/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20170906.838" "A Git porcelain inside Emacs"
+(define-package "magit" "20170907.1713" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20170219.942")
     (dash "20170207.2056")
