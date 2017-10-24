52aafe0a7a19c8431407289fba25ffcca6de2d32
Marc Sherry <msherry@gmail.com>
2017-10-24 12:01:05 -0700

    Package updates

--- elpa/dash-20171009.105/dash-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash" "20171009.105" "A modern list library for Emacs" 'nil :commit "8ef32875da21ea278002da823c05b7206cc1a81e" :keywords '("lists"))
---
+++ elpa/flycheck-pycheckers-20171011.2136/flycheck-pycheckers-pkg.el
@@ -0,0 +1,7 @@
+(define-package "flycheck-pycheckers" "20171011.2136" "multiple syntax checker for Python, using Flycheck"
+  '((flycheck "0.18"))
+  :url "https://github.com/msherry/flycheck-pycheckers" :keywords
+  '("convenience" "tools" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
