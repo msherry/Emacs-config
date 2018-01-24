35aa59f39175afb469d27e10da83a3a4a8e18f5e
Marc Sherry <msherry@gmail.com>
2018-01-24 12:21:25 -0800

    Package updates

---
+++ elpa/find-file-in-project-20180131.1749/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20180131.1749" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.3")) :commit "4c0f16bc5d97dd4132c63535a05de08904e9dce8" :url "https://github.com/technomancy/find-file-in-project" :keywords '("project" "convenience"))
