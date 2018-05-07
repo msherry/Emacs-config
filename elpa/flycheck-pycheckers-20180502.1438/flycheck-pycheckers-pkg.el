741ab6bf0028c48508d3e26efac16f7400da5c55
Marc Sherry <msherry@gmail.com>
2018-05-07 12:32:45 -0700

    Update flycheck-pycheckers

--- elpa/flycheck-pycheckers-20180502.1438/flycheck-pycheckers-pkg.el
+++ elpa/flycheck-pycheckers-20180502.1438/flycheck-pycheckers-pkg.el
@@ -1,4 +1,4 @@
-(define-package "flycheck-pycheckers" "20180402.1039" "multiple syntax checker for Python, using Flycheck"
+(define-package "flycheck-pycheckers" "20180502.1438" "multiple syntax checker for Python, using Flycheck"
   '((flycheck "0.18"))
   :keywords
   '("convenience" "tools" "languages")
