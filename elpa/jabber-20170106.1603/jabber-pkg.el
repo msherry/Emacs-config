db9005320f029908c99c99f81ce2b89e12750a45
Marc Sherry <msherry@gmail.com>
2018-06-01 13:38:03 -0700

    Package updates

--- elpa/jabber-20170106.1603/jabber-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "jabber" "20170106.1603" "A Jabber client for Emacs."
-  '((fsm "0.2")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/jabber-20170423.513/jabber-pkg.el
@@ -0,0 +1,5 @@
+(define-package "jabber" "20170423.513" "A Jabber client for Emacs."
+  '((fsm "0.2")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
