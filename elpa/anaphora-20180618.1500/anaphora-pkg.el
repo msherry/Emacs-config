e9b4eed66c42951a0a0e102270a9a46c2aec7048
Marc Sherry <msherry@gmail.com>
2018-07-12 12:17:23 -0700

    Package updates

---
+++ elpa/anaphora-20180618.1500/anaphora-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "anaphora" "20180618.1500" "anaphoric macros providing implicit temp variables" 'nil)
---
+++ elpa/hl-todo-20180709.2155/hl-todo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hl-todo" "20180709.2155" "highlight TODO and similar keywords" 'nil)
