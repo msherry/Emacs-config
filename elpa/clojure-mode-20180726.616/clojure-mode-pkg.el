131f2a6131096c7c5d5e8c7b42db87028400e4ce
Marc Sherry <msherry@gmail.com>
2018-08-03 13:11:50 -0700

    Package updates

--- elpa/clojure-mode-20180726.616/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20180726.616" "Major mode for Clojure code" '((emacs "25.1")) :commit "396f93e7d2266027b93131716f02a8e3d5663d89" :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
