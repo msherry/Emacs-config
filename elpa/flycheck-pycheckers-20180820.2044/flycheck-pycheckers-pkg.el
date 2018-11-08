47b4c25b58325b873906086f15353c7a62bcf360
Marc Sherry <marc.sherry@affirm.com>
2018-11-08 11:19:34 -0800

    Package updates

--- elpa/flycheck-pycheckers-20180820.2044/flycheck-pycheckers-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "flycheck-pycheckers" "20180820.2044" "multiple syntax checker for Python, using Flycheck"
-  '((flycheck "0.18"))
-  :keywords
-  '("convenience" "tools" "languages")
-  :url "https://github.com/msherry/flycheck-pycheckers")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/flycheck-pycheckers-20181108.1846/flycheck-pycheckers-pkg.el
@@ -0,0 +1,8 @@
+(define-package "flycheck-pycheckers" "20181108.1846" "multiple syntax checker for Python, using Flycheck"
+  '((flycheck "0.18"))
+  :keywords
+  '("convenience" "tools" "languages")
+  :url "https://github.com/msherry/flycheck-pycheckers")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
