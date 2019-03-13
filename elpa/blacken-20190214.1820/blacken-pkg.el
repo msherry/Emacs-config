c2f961bb3ca5b87bbec09cf2b4d3d70d59156144
Marc Sherry <marc.sherry@affirm.com>
2019-03-13 12:47:53 -0700

    Package updates

--- elpa/blacken-20190214.1820/blacken-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "blacken" "20190214.1820" "Reformat python buffers using the \"black\" formatter" '((emacs "25.2")) :commit "1dd9d18c7defbac58717eb62118991d7946310a8" :authors '(("Artem Malyshev" . "proofit404@gmail.com")) :maintainer '("Artem Malyshev" . "proofit404@gmail.com") :url "https://github.com/proofit404/blacken")
--- elpa/rust-mode-20190125.2135/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20190125.2135" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "54a9c3d3f501620cdf3f2d86f239571c1cc87fc6" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
