f9c4c68f1b0cd5cf7dd799eb498d772fe581ab67
Marc Sherry <marc.sherry@affirm.com>
2019-04-02 10:55:43 -0700

    Package updates

---
+++ elpa/ack-1.8/ack-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from ack.el
+(define-package "ack" "1.8" "interface to ack-like tools" 'nil :url "https://github.com/leoliu/ack-el" :keywords '("tools" "processes" "convenience"))
---
+++ elpa/projectile-20190326.759/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile" "20190326.759" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "21b2baa12b08443d5443f111dae233b5250e992f" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
