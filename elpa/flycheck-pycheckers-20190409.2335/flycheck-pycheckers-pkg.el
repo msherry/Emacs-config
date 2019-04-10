0d4c6ec4c4519f41acedbb0ea63a3cf67ae900bc
Marc Sherry <marc.sherry@affirm.com>
2019-04-10 14:26:31 -0700

    Package updates

---
+++ elpa/flycheck-pycheckers-20190409.2335/flycheck-pycheckers-pkg.el
@@ -0,0 +1,8 @@
+(define-package "flycheck-pycheckers" "20190409.2335" "multiple syntax checker for Python, using Flycheck"
+  '((flycheck "0.18"))
+  :keywords
+  '("convenience" "tools" "languages")
+  :url "https://github.com/msherry/flycheck-pycheckers")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
