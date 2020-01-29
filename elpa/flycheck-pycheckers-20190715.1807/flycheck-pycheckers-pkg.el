309e68ae842bd5f91186bebd797bf5baece86f22
Marc Sherry <msherry@gmail.com>
2020-01-28 21:45:45 -0800

    Delete old packages

--- elpa/flycheck-pycheckers-20190715.1807/flycheck-pycheckers-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "flycheck-pycheckers" "20190715.1807" "multiple syntax checker for Python, using Flycheck"
-  '((flycheck "0.18"))
-  :keywords
-  '("convenience" "tools" "languages")
-  :url "https://github.com/msherry/flycheck-pycheckers")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
