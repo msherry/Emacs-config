358b0221b049c2b57156eea4450c5d07c3c491f3
Marc Sherry <msherry@gmail.com>
2021-05-28 21:37:36 -0700

    Fix missing package

--- plugins/custom-faces.el
+++ plugins/custom-faces.el
@@ -5,11 +5,11 @@
 
 ;; Default "orange" color doesn't show up nicely if we don't have >8 colors, so
 ;; pick something that stands out more if we're color-impaired
-(when (<= (length (list-colors-duplicates (defined-colors))) 8)
-  (eval-after-load 'js2-mode
-    '(progn
-      (set-face-foreground 'js2-external-variable-face "white")
-      (set-face-background 'js2-external-variable-face "red"))))
+;; (when (<= (length (list-colors-duplicates (defined-colors))) 8)
+;;   (eval-after-load 'js2-mode
+;;     '(progn
+;;       (set-face-foreground 'js2-external-variable-face "white")
+;;       (set-face-background 'js2-external-variable-face "red"))))
 
 ; Try to highlight "TODO: " entries
 (defface todo-face
