cc8cbc2d40a40c5fe5f5deacf3e60a1955ea4529
Marc Sherry <marc.sherry@affirm.com>
2021-06-01 16:25:29 -0700

    Package updates

---
+++ elpa/ido-completing-read+-20210529.1318/ido-completing-read+-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ido-completing-read+.el  -*- no-byte-compile: t -*-
+(define-package "ido-completing-read+" "20210529.1318" "A completing-read-function using ido" '((emacs "24.4") (seq "0.5") (memoize "1.1")) :commit "00674721e4fce283c918f7316f1158da1d469910" :authors '(("Ryan C. Thompson" . "rct@thompsonclan.org")) :maintainer '("Ryan C. Thompson" . "rct@thompsonclan.org") :keywords '("ido" "completion" "convenience") :url "https://github.com/DarwinAwardWinner/ido-completing-read-plus")
--- elpa/projectile-20210527.642/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from projectile.el  -*- no-byte-compile: t -*-
-(define-package "projectile" "20210527.642" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "4e22746dcf8d6dcec1122766c966db776a57a2ef" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :keywords '("project" "convenience") :url "https://github.com/bbatsov/projectile")
