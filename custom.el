eb3e8135d2acbb77c79f523a1d889bbd1b4750e7
Marc Sherry <marc.sherry@affirm.com>
2021-06-01 15:24:23 -0700

    Restore `ack` functionality

--- custom.el
+++ custom.el
@@ -32,6 +32,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ack-command "ag --ignore tests ")
+ '(ack-defaults-function 'ack-legacy-defaults)
  '(appt-delete-window-function (lambda nil))
  '(appt-disp-window-function 'msherry/appt-disp-window)
  '(appt-display-format 'window)
