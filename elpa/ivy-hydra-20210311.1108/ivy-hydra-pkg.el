8dae457511ee678618e2bd304d10ff6d09328bf8
Marc Sherry <marc.sherry@affirm.com>
2021-06-07 15:19:02 -0700

    Add ivy-hydra

---
+++ elpa/ivy-hydra-20210311.1108/ivy-hydra-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ivy-hydra.el  -*- no-byte-compile: t -*-
+(define-package "ivy-hydra" "20210311.1108" "Additional key bindings for Ivy" '((emacs "24.5") (ivy "0.13.4") (hydra "0.14.0")) :commit "a5eade0c77b7dd78cdea0f246683c6cb00f7b7c6" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("convenience") :url "https://github.com/abo-abo/swiper")
