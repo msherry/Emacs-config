daff856abce18ee5d517562bfd7dcdd9c3031a99
Marc Sherry <marc.sherry@affirm.com>
2021-06-16 17:31:17 -0700

    Package updates

---
+++ elpa/merlin-20210615.1208/merlin-pkg.el
@@ -0,0 +1,12 @@
+(define-package "merlin" "20210615.1208" "Mode for Merlin, an assistant for OCaml"
+  '((emacs "25.1"))
+  :commit "3eafee3ebb399666db04b3046880e61030126e41" :authors
+  '(("Frédéric Bour <frederic.bour(_)lakaban.net>"))
+  :maintainer
+  '("Frédéric Bour <frederic.bour(_)lakaban.net>")
+  :keywords
+  '("ocaml" "languages")
+  :url "https://github.com/ocaml/merlin")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/which-key-20210615.1719/which-key-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from which-key.el  -*- no-byte-compile: t -*-
+(define-package "which-key" "20210615.1719" "Display available keybindings in popup" '((emacs "24.4")) :commit "12f743c6955c89352f8d49d292895bd45e82d26f" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
