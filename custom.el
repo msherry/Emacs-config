1ac1d05d7de4f92d04bc3b009c52b399f4719307
Marc Sherry <marc.sherry@affirm.com>
2022-03-14 10:41:28 -0700

    New notmuch search, tweaks

--- custom.el
+++ custom.el
@@ -123,7 +123,8 @@
      (:name "inbox (personal)" :query "tag:INBOX and tag:personal" :key "l")
      (:name "unread (personal)" :query "tag:unread AND tag:INBOX AND tag:personal" :key "p")
      (:name "all mail (affirm)" :query "tag:affirm" :key "a")
-     (:name "all mail (personal)" :query "tag:personal" :key "m")))
+     (:name "all mail (personal)" :query "tag:personal" :key "m")
+     (:name "Sent mail" :query "tag:sent")))
  '(notmuch-search-oldest-first nil)
  '(ns-alternate-modifier 'super)
  '(ns-command-modifier 'meta)
@@ -302,7 +303,7 @@ Train
  '(org-todo-keyword-faces '(("BLOCKED" . "#586e75") ("IN_QUEUE" . "#586e75")))
  '(org-use-sub-superscripts '{})
  '(package-selected-packages
-   '(ac-geiser ack arduino-cli-mode arduino-mode auctex auto-minor-mode bazel-mode blacken cargo cider clojure-mode clojure-mode-extra-font-locking common-lisp-snippets counsel diff-hl dockerfile-mode dtrace-script-mode el2markdown elpy emojify ess esup exec-path-from-shell feature-mode find-find-in-project flx flx-ido flycheck-clojure flycheck-ocaml flycheck-package flycheck-pycheckers flycheck-rust fxrd-mode geiser geiser-guile geiser-racket git-modes go-mode graphviz-dot-mode groovy-mode helm-projectile httpcode ivy jedi json-mode latex-preview-pane magit magithub markdown-preview-mode notmuch org-agenda-property org-jira org-mru-clock org-plus-contrib org-pomodoro org-super-agenda ox-gfm package-lint paredit php-mode pinentry projectile protobuf-mode pymacs python-mode racer racket-mode rainbow-mode realgud-ipdb rmsbolt rust-mode sass-mode slime smtpmail-multi solarized-theme suggest swift-mode tagedit thrift tickscript-mode tuareg virtualenv which-key window-numbering yaml-mode yasnippet-snippets))
+   '(ac-geiser ack arduino-cli-mode arduino-mode auctex auto-minor-mode bazel-mode blacken cargo cider clojure-mode clojure-mode-extra-font-locking common-lisp-snippets counsel diff-hl dockerfile-mode dtrace-script-mode el2markdown elpy emojify ess esup exec-path-from-shell feature-mode find-find-in-project flx flx-ido flycheck-clojure flycheck-ocaml flycheck-package flycheck-pycheckers flycheck-rust fxrd-mode geiser geiser-guile geiser-racket git-modes go-mode graphviz-dot-mode groovy-mode helm-projectile httpcode ido-completing-read+ ivy jabber jedi json-mode julia-mode kotlin-mode latex-preview-pane magit magithub markdown-preview-mode notmuch oauth2 org org-agenda-property org-jira org-mru-clock org-plus-contrib org-pomodoro org-super-agenda ox-gfm package-lint paredit php-mode pinentry protobuf-mode puppet-mode pymacs python-mode racer racket-mode rainbow-mode realgud-ipdb rmsbolt rust-mode s sass-mode slime smex smtpmail-multi solarized-theme suggest swift-mode tagedit thrift tickscript-mode tuareg virtualenv which-key window-numbering yaml-mode yasnippet-snippets))
  '(projectile-completion-system 'ivy)
  '(projectile-enable-caching t)
  '(projectile-globally-ignored-modes
