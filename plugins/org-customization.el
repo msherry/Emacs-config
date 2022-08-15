623da890dae14ba065bff5283637b0ebda21f28f
Marc Sherry <msherry@gmail.com>
2022-08-15 10:47:06 -0700

    delete empty sections in org agenda

--- plugins/org-customization.el
+++ plugins/org-customization.el
@@ -631,5 +631,42 @@ works as I expect it to when calling this programmatically."
 			(org-babel-hide-result-toggle-maybe))))))
 
 
+;; https://emacs.stackexchange.com/a/33391/7169
+(defun org-agenda-delete-empty-blocks ()
+  "Remove empty agenda blocks.
+  A block is identified as empty if there are fewer than 2
+  non-empty lines in the block (excluding the line with
+  `org-agenda-block-separator' characters)."
+  (when org-agenda-compact-blocks
+    (user-error "Cannot delete empty compact blocks"))
+  (setq buffer-read-only nil)
+  (save-excursion
+    (goto-char (point-min))
+    (let* ((blank-line-re "^\\s-*$")
+           (content-line-count (if (looking-at-p blank-line-re) 0 1))
+           (start-pos (point))
+           (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+      (while (and (not (eobp)) (forward-line))
+        (cond
+          ((looking-at-p block-re)
+           (when (< content-line-count 2)
+             (delete-region start-pos (1+ (point-at-bol))))
+           (setq start-pos (point))
+           (forward-line)
+           (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+          ((not (looking-at-p blank-line-re))
+           (setq content-line-count (1+ content-line-count)))))
+      (when (< content-line-count 2)
+        (delete-region start-pos (point-max)))
+      (goto-char (point-min))
+      ;; The above strategy can leave a separator line at the beginning
+      ;; of the buffer.
+      (when (looking-at-p block-re)
+        (delete-region (point) (1+ (point-at-eol))))))
+  (setq buffer-read-only t))
+
+(add-hook 'org-agenda-finalize-hook #'org-agenda-delete-empty-blocks)
+
+
 (provide 'org-customization)
 ;;; org-customization.el ends here
