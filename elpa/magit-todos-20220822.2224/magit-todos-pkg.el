1f02c213ee8389d958d407b8d88e576730cadad2
Marc Sherry <marc.sherry@affirm.com>
2022-11-30 19:23:43 -0800

    add magit-todos-mode

---
+++ elpa/magit-todos-20220822.2224/magit-todos-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from magit-todos.el  -*- no-byte-compile: t -*-
+(define-package "magit-todos" "20220822.2224" "Show source file TODOs in Magit" '((emacs "26.1") (async "1.9.2") (dash "2.13.0") (f "0.17.2") (hl-todo "1.9.0") (magit "2.13.0") (pcre2el "1.8") (s "1.12.0") (transient "0.2.0")) :commit "c5030cc27c7c1a48db52b0134bf2648a59a43176" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("magit" "vc") :url "http://github.com/alphapapa/magit-todos")
