5466d0f9df33cf4b6b623e20994b90ef0f848aa5
Marc Sherry <marc.sherry@affirm.com>
2023-01-06 12:22:58 -0800

    package upgrades

--- elpa/clojure-mode-extra-font-locking-20211230.817/clojure-mode-extra-font-locking-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from clojure-mode-extra-font-locking.el  -*- no-byte-compile: t -*-
-(define-package "clojure-mode-extra-font-locking" "20211230.817" "Extra font-locking for Clojure mode" '((clojure-mode "3.0")) :commit "913e2450a77a6ddda051f55ac651c99337147db1" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
