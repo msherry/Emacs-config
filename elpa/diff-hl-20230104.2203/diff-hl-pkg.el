df9a62c743d62dbd4a795a4f9e0f6caa019e0c7e
Marc Sherry <marc.sherry@affirm.com>
2023-01-06 12:26:12 -0800

    package upgrades

---
+++ elpa/diff-hl-20230104.2203/diff-hl-pkg.el
@@ -0,0 +1,13 @@
+(define-package "diff-hl" "20230104.2203" "Highlight uncommitted changes using VC"
+  '((cl-lib "0.2")
+    (emacs "25.1"))
+  :commit "8f2e4eb345f0639c8fc41e3f7576f77ba6987655" :authors
+  '(("Dmitry Gutov" . "dgutov@yandex.ru"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("vc" "diff")
+  :url "https://github.com/dgutov/diff-hl")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/flycheck-pycheckers-20211122.235/flycheck-pycheckers-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "flycheck-pycheckers" "20211122.235" "multiple syntax checker for Python, using Flycheck"
-  '((flycheck "0.18"))
-  :commit "56965c0ef5d45bcef90093360718c6967ce4ef39" :keywords
-  '("convenience" "tools" "languages")
-  :url "https://github.com/msherry/flycheck-pycheckers")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
