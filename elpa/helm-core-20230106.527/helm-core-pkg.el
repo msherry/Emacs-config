62afa87e846432f1eea8a5a5abf04819f976908c
Marc Sherry <marc.sherry@affirm.com>
2023-01-06 13:18:24 -0800

    package upgrades

---
+++ elpa/helm-core-20230106.527/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20230106.527" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "4e39df9ee024f82548028f0edde9c71afbcd3635" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
