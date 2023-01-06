d0e175dd52872fce5b20931cb416f1d66f115ca8
Marc Sherry <marc.sherry@affirm.com>
2023-01-06 12:21:47 -0800

    package upgrades

---
+++ elpa/cargo-20221025.810/cargo-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cargo" "20221025.810" "Emacs Minor Mode for Cargo, Rust's Package Manager."
+  '((emacs "24.3")
+    (markdown-mode "2.4"))
+  :commit "d2720c8dc7ac3b18ce112a886d3b8696797d01cb" :authors
+  '(("Kevin W. van Rooijen"))
+  :maintainer
+  '("Kevin W. van Rooijen")
+  :keywords
+  '("tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
