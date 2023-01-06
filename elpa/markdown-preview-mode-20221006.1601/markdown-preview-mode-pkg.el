fd79c2af7c8b06fee7e64187749926e1f82a7796
Marc Sherry <marc.sherry@affirm.com>
2023-01-06 13:06:12 -0800

    package upgrades

---
+++ elpa/markdown-preview-mode-20221006.1601/markdown-preview-mode-pkg.el
@@ -0,0 +1,16 @@
+(define-package "markdown-preview-mode" "20221006.1601" "markdown realtime preview minor mode."
+  '((emacs "24.4")
+    (websocket "1.6")
+    (markdown-mode "2.0")
+    (cl-lib "0.5")
+    (web-server "0.1.1"))
+  :commit "85fbfec507a222326695a3b91ff7306d0c4f94c6" :authors
+  '(("Igor Shymko" . "igor.shimko@gmail.com"))
+  :maintainer
+  '("Igor Shymko" . "igor.shimko@gmail.com")
+  :keywords
+  '("markdown" "gfm" "convenience")
+  :url "https://github.com/ancane/markdown-preview-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/notmuch-20221115.1134/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20221115.1134" "run notmuch within emacs" 'nil :commit "5ba7684445b9338ab289d502d4688d9aff3d8803" :url "https://notmuchmail.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
